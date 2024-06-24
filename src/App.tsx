b614756bda33735473d12ea873dad5915e313d06
piusgori <piusgori@gmail.com>
2024-06-24 09:35:09 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Hero from "./components/hero"
+import Highlights from "./components/highlights"
+import Navbar from "./components/navbar"
+
+
+function App() {
+
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  )
+}
+
+export default App
